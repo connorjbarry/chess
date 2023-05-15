37a4f351e80d9c33b44afbd303dcc9de0bfb0743
connorjbarry <barry31@purdue.edu>
2023-05-15 13:44:14 -0500

    modularized the board

---
+++ src/board.rs
@@ -0,0 +1,31 @@
+use bevy::prelude::*;
+
+pub struct Board {}
+
+impl Board {
+    pub fn board_setup(mut commands: Commands) {
+        commands.spawn(Camera2dBundle::default());
+    
+        let square_size = 50;
+    
+        for x in 0..8 {
+            for y in 0..8 {
+                let color = if (x + y) % 2 == 0 {
+                    Color::WHITE
+                } else {
+                    Color::BLACK 
+                };
+    
+                commands.spawn(SpriteBundle {
+                    sprite: Sprite {
+                        color,
+                        custom_size: Some(Vec2::new(square_size as f32, square_size as f32)),
+                        ..default()
+                    },
+                    transform: Transform::from_translation(Vec3::new((x * square_size - 200) as f32, (y * square_size - 150) as f32, 0.0)),
+                    ..Default::default()
+                });
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+mod board;
+use crate::board::Board;
+
 use bevy::prelude::*;
 
 fn main() {
@@ -10,32 +13,14 @@ fn main() {
         }),
         ..default()
     }))
-    .add_startup_system(board_setup)
+    .add_plugin(BoardPlugin)
     .run();
 }
 
-fn board_setup(mut commands: Commands) {
-    commands.spawn(Camera2dBundle::default());
+pub struct BoardPlugin;
 
-    let square_size = 50;
-
-    for x in 0..8 {
-        for y in 0..8 {
-            let color = if (x + y) % 2 == 0 {
-                Color::WHITE
-            } else {
-                Color::BLACK 
-            };
-
-            commands.spawn(SpriteBundle {
-                sprite: Sprite {
-                    color,
-                    custom_size: Some(Vec2::new(square_size as f32, square_size as f32)),
-                    ..default()
-                },
-                transform: Transform::from_translation(Vec3::new((x * square_size - 200) as f32, (y * square_size - 150) as f32, 0.0)),
-                ..Default::default()
-            });
-        }
+impl Plugin for BoardPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_startup_system(Board::board_setup);
     }
 }
