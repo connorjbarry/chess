0a5edb8d25d6d4b2f58e8606a12e9b8296385ee7
connorjbarry <barry31@purdue.edu>
2023-05-18 21:49:32 -0500

    added tests to the fen john

--- src/fen.rs
+++ src/fen.rs
@@ -9,23 +9,25 @@ pub fn get_board_from_fen(fen: &str) -> Vec<u32> {
 
     let mut board: Vec<u32> = vec![0; 64];
 
-    let mut rank: u32 = 7;
+    let mut rank: u32 = 0;
     let mut file: u32 = 0;
 
     for c in fen_board.chars() {
         if c == '/' {
-            rank -= 1;
+            rank += 1;
             file = 0;
-        } else if c.is_digit(10) {
-            file += c.to_digit(10).unwrap();
         } else {
-            let piece = get_piece(c);
-            board[(rank * 8 + file) as usize] = piece;
-            file += 1;
+            if c.is_digit(10) {
+                file += c.to_digit(10).unwrap();
+            } else {
+                let piece = get_piece(c);
+                board[(rank * 8 + file) as usize] = piece;
+                file += 1;
+            }
         }
     }
 
-    vec![0; 64]
+    board
 
 }
 
@@ -49,4 +51,28 @@ fn get_piece(letter: char) -> u32 {
     };
 
     piece_color | piece_type
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_pawn_conversion() {
+        let piece_from_char = get_piece("p".chars().next().unwrap());
+        assert_eq!(piece_from_char, 18);
+    } 
+    #[test] 
+    fn test_rook_conversion() {
+        let piece_from_char = get_piece("r".chars().next().unwrap());
+        assert_eq!(piece_from_char, 21);
+    }
+
+    #[test]
+    fn test_board_from_fen() {
+        let board_from_fen = get_board_from_fen(STARTING_FEN);
+        assert_eq!(board_from_fen.len(), 64);
+        assert_eq!(board_from_fen[0], 21);
+        assert_eq!(board_from_fen[63], 13);
+    }
 }
